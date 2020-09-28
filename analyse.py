09b4a86a133ddc1bc5d708ee49135dcfab0c60e3
Renata Nésio <renata.nesio@live.com>
2020-09-28 10:45:47 -0300

    first commit

---
+++ analyse.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+
+df = pd.read_csv("data.csv", index_col=0)
+
+print("Preview of dataframe")
+print(df.head())
+
+print("Describe first feature")
+print(df['1'].describe())
+
---
+++ get_data.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from sklearn.datasets import make_regression
+import pandas as pd
+import os
+import numpy as np
+
+#By Elle O'Brien
+
+# If there's no dataset in the project directory, create a reasonably large one. 
+# If it exists, append some new observations. 
+if os.path.isfile("data.csv"):
+    n = 1
+else:
+    n = 50
+
+for i in range(0,n):    
+    X, y = make_regression(10000,n_features = 10)
+    df = pd.DataFrame(X)
+    df.to_csv("data.csv",mode='a')
+
+
+# In[ ]:
+
+
+
+
